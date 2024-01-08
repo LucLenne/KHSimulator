24b99605b0e33f2b6f9ace5acb5fa52f323a990a
LucLenne <113776725+LucLenne@users.noreply.github.com>
2024-01-08 17:55:15 +0100

    debut input

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -8,11 +8,13 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using static UnityEngine.InputSystem.InputSettings;
 
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] InputActionReference _move;
     [SerializeField] float _speed;
+    private PlayerInput _playerInput;
 
     // Event pour les dev
     public event Action OnStartMove;
@@ -25,14 +27,37 @@ public class PlayerMove : MonoBehaviour
     public Vector2 JoystickDirection { get; private set; }
     Coroutine MovementRoutine { get; set; }
 
+    private void Awake()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+        _playerInput.actions["Move"].ReadValue<Vector2>();
+    }
+
+
     private void Start()
     {
-        
+        _move.action.started += StartMove;
+        _move.action.performed += UpdateMove;
+        _move.action.started += StopMove;
     }
 
     private void OnDestroy()
     {
-        
+        _move.action.started -= StartMove;
+        _move.action.performed -= UpdateMove;
+        _move.action.started -= StopMove;
     }
 
+    private void StartMove(InputAction.CallbackContext context)
+    {
+        OnStartMove?.Invoke();
+    }
+    private void UpdateMove(InputAction.CallbackContext context)
+    {
+
+    }
+    private void StopMove(InputAction.CallbackContext context)
+    {
+
+    }
 }
